5f039c77b9bca38d9754e5b50b262b10dc7d20d0
Dreadful0 <slavadem100500@gmail.com>
2017-11-07 18:51:39 +0200

    Added default project organization with input

---
+++ lab1.task3/src/main/java/com/epam/lab1/task3/start/Main.java
@@ -0,0 +1,16 @@
+package com.epam.lab1.task3.start;
+
+import com.epam.lab1.task3.back.App;
+
+/**
+ * @author Viacheslav Demianenko
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+    	App app = new App();
+		app.doWork();
+    }
+}
