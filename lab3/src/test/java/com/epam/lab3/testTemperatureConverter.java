819ae4ccf85616ea25f18e3fe5180350695d54d6
Dreadful0 <slavadem100500@gmail.com>
2017-11-17 13:17:41 +0200

    Added parameterized test

---
+++ lab3/src/test/java/com/epam/lab3/testTemperatureConverter.java
@@ -0,0 +1,89 @@
+package com.epam.lab3;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.epam.lab3.back.TemperatureConverter;
+
+@RunWith(value = Parameterized.class)
+public class testTemperatureConverter {
+
+	@Parameter(0)
+	public double celsius;
+	@Parameter(1)
+	public double kelvin;
+	@Parameter(2)
+	public double fahrenheit;
+	
+	public static TemperatureConverter conv;
+	
+	@Parameters(name="Test C:{0}, K:{1}, F:{2}")
+	public static Collection<Object[]> getTestData(){
+		return Arrays.asList(new Object[][]{
+				{0, 273.2, 32},
+		});
+	}
+	
+	public testTemperatureConverter() {
+	}
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		conv = new TemperatureConverter();
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+		conv = null;
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testConvertFtoC() {
+		assertEquals(celsius, conv.convertFtoC(fahrenheit), 0.1);
+	}
+
+	@Test
+	public void testConvertCtoF() {
+		assertEquals(fahrenheit, conv.convertCtoF(celsius), 0.1);
+	}
+
+	@Test
+	public void testConvertCtoK() {
+		assertEquals(kelvin, conv.convertCtoK(celsius), 0.1);
+	}
+
+	@Test
+	public void testConvertKtoC() {
+		assertEquals(celsius, conv.convertKtoC(kelvin), 0.1);
+	}
+
+	@Test
+	public void testConvertFtoK() {
+		assertEquals(kelvin, conv.convertFtoK(fahrenheit), 0.1);
+	}
+
+	@Test
+	public void testConvertKtoF() {
+		assertEquals(fahrenheit, conv.convertKtoF(kelvin), 0.1);
+	}
+
+}
